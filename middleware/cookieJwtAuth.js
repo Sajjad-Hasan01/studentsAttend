f4470181d8f1d17c58de7ceb48df6bb0080b17f6
Sajjad Hasan <sajadph375@gmail.com>
2023-10-13 19:16:26 +0300

    ...

--- middleware/cookieJwtAuth.js
+++ middleware/cookieJwtAuth.js
@@ -4,9 +4,10 @@ exports.cookieJwtAuth = async (req, res, next) => {
     const accessToken = await req.signedCookies["access-token"];
     
     try {
-        const {id} = jwt.verify(accessToken, process.env.SECRET);
-        req.id = id;
-        next(req, res);
+        return res.send(accessToken);
+        // const {id} = jwt.verify(accessToken, process.env.SECRET);
+        // req.id = id;
+        // next(req, res);
     } catch (error) {
         return res.clearCookie("access-token").status(403).send(error);
     }
